ba49e9bf3ae4b5b4e4d7195b5bba4cc6f91a4f0b
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-09-18 08:45:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -14,7 +14,7 @@ For more click help....</b>"""
 <code>🌀 Your Edited Image will be Uploaded </code>
 
 
-© @Mo_Tech_YT"""
+© @SweetAngelUpdates"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
